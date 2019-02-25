fd40a4fbb0e5bcff9e9f376970ded6d4d7ae91db
Tomáš Podolský <podolsky.tomas@gmail.com>
2019-02-25 20:44:18 +0100

    validace dat insert metody

--- app/src/main/java/com/example/workhours/Shift.java
+++ app/src/main/java/com/example/workhours/Shift.java
@@ -38,6 +38,7 @@ public class Shift extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
+
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()){
             case (android.R.id.home):
